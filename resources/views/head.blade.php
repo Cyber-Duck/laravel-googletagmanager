f97b328e9cdeb5964c4ffb4dc93935d94d7d999f
Wai Yan Hein <wai@cyber-duck.co.uk>
2019-10-09 09:51:30 +0100

    Fixed the GTM script

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -7,8 +7,8 @@ dataLayer.push({!! $item->toJson() !!});
 @endforeach
 </script>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl{{ ($envEnabled)? '&gtm_auth=' . $envAuth . '&gtm_preview=' . $envPreview : '' }};f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{{ $id }}');</script>
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl+'{{ ($envEnabled)? '&gtm_auth=' . $envAuth . '&gtm_preview=' . $envPreview : '' }}';f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','{{ $id }}');</script>
 @endif
