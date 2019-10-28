2de2e11ca02808ee2218e573505fc9db198b794e
Wai Yan Hein <wai@cyber-duck.co.uk>
2019-10-28 10:57:38 +0000

    Fixed the snippet error appeared on the Tag Assistant. Also changed the snippet for the frame in the body to support multiple environments

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -1,4 +1,6 @@
 @if($enabled)
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $id }}"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $id }}&gtm_preview={{ $envPreview }}&gtm_cookies_win=x"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 @endif
--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,14 +1,19 @@
 @if($enabled)
-<script>
-window.dataLayer = window.dataLayer || [];
-dataLayer = [{!! $dataLayer->toJson() !!}];
-@foreach($pushData as $item)
-dataLayer.push({!! $item->toJson() !!});
-@endforeach
-</script>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl+'{{ ($envEnabled)? '&gtm_auth=' . $envAuth . '&gtm_preview=' . $envPreview : '' }}';f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','{{ $id }}');</script>
+    <script nonce="{{ get_nonce() }}">
+        window.dataLayer = window.dataLayer || []
+        @if(!empty($dataLayer->toArray()))
+            dataLayer = [{!! $dataLayer->toJson() !!}];
+        @endif
+        @foreach($pushData as $item)
+        dataLayer.push({!! $item->toJson() !!});
+        @endforeach
+    </script>
+
+    <!-- Google Tag Manager -->
+    <script nonce="{{ get_nonce() }}">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth={{ $envAuth }}&gtm_preview={{ $envPreview }}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','{{ $id }}');</script>
+    <!-- End Google Tag Manager -->
 @endif
